b9654d8891c8fd6e4bdbf401407739921d5a11bf
Jutho <jutho.haegeman@ugent.be>
2023-07-27 10:21:29 +0200

    change `@constinferred` parsing to make anonymous functions instead of using gensym names

--- src/constinferred.jl
+++ src/constinferred.jl
@@ -32,14 +32,16 @@ module ConstInferred
     """
         @constinferred [AllowedType] f(x)
     Tests that the call expression `f(x)` returns a value of the same type inferred by the
-    compiler. It is useful to check for type stability. Similar to `Test.@inferred`, but differs in two important ways.
+    compiler. It is useful to check for type stability. Similar to `Test.@inferred`, but
+    differs in two important ways.
 
     Firstly, `@constinferred` tries to test type stability under constant propagation by
     testing type stability of a new function, where all arguments or keyword arguments of
     the original function `f` that have a constant value (in the call expression) of type
     `Union{Number,Char,Symbol}` are hard coded.
 
-    If you want to test for constant propagation in a variable which is not hard-coded in the call expression, you can interpolate it into the expression.
+    If you want to test for constant propagation in a variable which is not hard-coded in 
+    the call expression, you can interpolate it into the expression.
 
     Secondly, @constinferred returns the value if type inference succeeds, like `@inferred`,
     but used the `Test.@test` mechanism and shows up as an actual test error when type
@@ -127,11 +129,12 @@ module ConstInferred
             end
         end
         f = Expr(:$, ex.args[1])
-        fundefhead = Expr(:call, newf, leftargs...)
+        
+        fundefhead = Expr(:tuple, leftargs...)
         fundefbody = Expr(:block, quoteargs..., isempty(kwargs) ?
                             Expr(:call, f, rightargs...) :
                             Expr(:call, f, Expr(:parameters, rightkwargs...), rightargs...))
-        fundefex = esc(Expr(:quote, Expr(:(=), fundefhead, fundefbody)))
+        fundefex = esc(Expr(:quote, Expr(:(=), newf, Expr(:->, fundefhead, fundefbody))))
 
         inftypes = gensym()
         rettype = gensym()
