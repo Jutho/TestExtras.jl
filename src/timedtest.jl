dd0d3d18598a8da941fb446900829cd7e417442a
Jutho <jutho.haegeman@ugent.be>
2023-07-27 10:37:56 +0200

    add formatting

--- src/timedtest.jl
+++ src/timedtest.jl
@@ -1,11 +1,10 @@
 module TimedTests
 export @timedtestset, TimedTestSet
 
-import Test
+using Test: Test
 using Test: AbstractTestSet, DefaultTestSet, Broken, Fail, Error, Pass, TestSetException
 import Test: record, finish, print_test_errors, print_test_results, print_counts,
-            get_testset, get_testset_depth,get_test_counts, get_alignment, filter_errors
-
+             get_testset, get_testset_depth, get_test_counts, get_alignment, filter_errors
 
 macro timedtestset(ex...)
     if length(ex) == 2
@@ -19,8 +18,9 @@ macro timedtestset(ex...)
     # for some reason, you cannot do @testset TestExtras.TimedTests.TimedTestSet begin ...
     # instead we do: var = TestExtras.TimedTests.TimedTestSet; @testset var begin ...
     return esc(Expr(:block,
-    Expr(:(=), timedtestsetvar, :($TimedTestSet)),
-    Expr(:macrocall, Symbol("@testset"), __source__, timedtestsetvar, name, testset)))
+                    Expr(:(=), timedtestsetvar, :($TimedTestSet)),
+                    Expr(:macrocall, Symbol("@testset"), __source__, timedtestsetvar, name,
+                         testset)))
 end
 
 @nospecialize
@@ -42,9 +42,9 @@ record(ts::TimedTestSet, t::Pass) = (ts.n_passed += 1; t)
 
 # For the other result types, immediately print the error message
 # but do not terminate. Print a backtrace.
-function record(ts::TimedTestSet, t::Union{Fail, Error})
+function record(ts::TimedTestSet, t::Union{Fail,Error})
     if Test.TESTSET_PRINT_ENABLE[]
-        printstyled(ts.description, ": ", color=:white)
+        printstyled(ts.description, ": "; color=:white)
         # don't print for interrupted tests
         if !(t isa Error) || t.test_type !== :test_interrupted
             print(t)
@@ -57,7 +57,7 @@ function record(ts::TimedTestSet, t::Union{Fail, Error})
         end
     end
     push!(ts.results, t)
-    t, isa(t, Error) || backtrace()
+    return t, isa(t, Error) || backtrace()
 end
 
 # When a TimedTestSet finishes, it records itself to its parent
@@ -83,23 +83,23 @@ function print_test_results(ts::TimedTestSet, depth_pad=0)
     # Calculate the overall number for each type so each of
     # the test result types are aligned
     passes, fails, errors, broken, c_passes, c_fails, c_errors, c_broken = get_test_counts(ts)
-    total_pass   = passes + c_passes
-    total_fail   = fails  + c_fails
-    total_error  = errors + c_errors
+    total_pass = passes + c_passes
+    total_fail = fails + c_fails
+    total_error = errors + c_errors
     total_broken = broken + c_broken
-    dig_pass   = total_pass   > 0 ? ndigits(total_pass)   : 0
-    dig_fail   = total_fail   > 0 ? ndigits(total_fail)   : 0
-    dig_error  = total_error  > 0 ? ndigits(total_error)  : 0
+    dig_pass = total_pass > 0 ? ndigits(total_pass) : 0
+    dig_fail = total_fail > 0 ? ndigits(total_fail) : 0
+    dig_error = total_error > 0 ? ndigits(total_error) : 0
     dig_broken = total_broken > 0 ? ndigits(total_broken) : 0
     total = total_pass + total_fail + total_error + total_broken
     dig_total = total > 0 ? ndigits(total) : 0
     # For each category, take max of digits and header width if there are
     # tests of that type
-    pass_width   = dig_pass   > 0 ? max(length("Pass"),   dig_pass)   : 0
-    fail_width   = dig_fail   > 0 ? max(length("Fail"),   dig_fail)   : 0
-    error_width  = dig_error  > 0 ? max(length("Error"),  dig_error)  : 0
+    pass_width = dig_pass > 0 ? max(length("Pass"), dig_pass) : 0
+    fail_width = dig_fail > 0 ? max(length("Fail"), dig_fail) : 0
+    error_width = dig_error > 0 ? max(length("Error"), dig_error) : 0
     broken_width = dig_broken > 0 ? max(length("Broken"), dig_broken) : 0
-    total_width  = dig_total  > 0 ? max(length("Total"),  dig_total)  : 0
+    total_width = dig_total > 0 ? max(length("Total"), dig_total) : 0
     # Calculate the alignment of the test result counts by
     # recursively walking the tree of test sets
     align = max(get_alignment(ts, 0), length("Test Summary:"))
@@ -110,24 +110,27 @@ function print_test_results(ts::TimedTestSet, depth_pad=0)
         printstyled(lpad("Pass", pass_width, " "), "  "; bold=true, color=:green)
     end
     if fail_width > 0
-        printstyled(lpad("Fail", fail_width, " "), "  "; bold=true, color=Base.error_color())
+        printstyled(lpad("Fail", fail_width, " "), "  "; bold=true,
+                    color=Base.error_color())
     end
     if error_width > 0
-        printstyled(lpad("Error", error_width, " "), "  "; bold=true, color=Base.error_color())
+        printstyled(lpad("Error", error_width, " "), "  "; bold=true,
+                    color=Base.error_color())
     end
     if broken_width > 0
-        printstyled(lpad("Broken", broken_width, " "), "  "; bold=true, color=Base.warn_color())
+        printstyled(lpad("Broken", broken_width, " "), "  "; bold=true,
+                    color=Base.warn_color())
     end
     if total_width > 0
         printstyled(lpad("Total", total_width, " "); bold=true, color=Base.info_color())
     end
-    printstyled("  Time: "; bold = true, color = Base.info_color())
-    printstyled(string(round(ts.tf-ts.ti; sigdigits=3)); color = Base.info_color())
+    printstyled("  Time: "; bold=true, color=Base.info_color())
+    printstyled(string(round(ts.tf - ts.ti; sigdigits=3)); color=Base.info_color())
     println()
     # Recursively print a summary at every level
-    print_counts(ts, depth_pad, align, pass_width, fail_width, error_width, broken_width, total_width)
+    return print_counts(ts, depth_pad, align, pass_width, fail_width, error_width,
+                        broken_width, total_width)
 end
-
 
 # Called at the end of a @testset, behaviour depends on whether
 # this is a child of another testset, or the "root" testset
@@ -142,9 +145,9 @@ function finish(ts::TimedTestSet)
         return ts
     end
     passes, fails, errors, broken, c_passes, c_fails, c_errors, c_broken = get_test_counts(ts)
-    total_pass   = passes + c_passes
-    total_fail   = fails  + c_fails
-    total_error  = errors + c_errors
+    total_pass = passes + c_passes
+    total_fail = fails + c_fails
+    total_error = errors + c_errors
     total_broken = broken + c_broken
     total = total_pass + total_fail + total_error + total_broken
 
@@ -160,7 +163,7 @@ function finish(ts::TimedTestSet)
     end
 
     # return the testset so it is returned from the @testset macro
-    ts
+    return ts
 end
 
 # Recursive function that finds the column that the result counts
@@ -170,13 +173,13 @@ end
 # in calculating the alignment
 function get_alignment(ts::TimedTestSet, depth::Int)
     # The minimum width at this depth is
-    ts_width = 2*depth + length(ts.description)
+    ts_width = 2 * depth + length(ts.description)
     # If all passing, no need to look at children
     !ts.anynonpass && return ts_width
     # Return the maximum of this width and the minimum width
     # for all children (if they exist)
     isempty(ts.results) && return ts_width
-    child_widths = map(t->get_alignment(t, depth+1), ts.results)
+    child_widths = map(t -> get_alignment(t, depth + 1), ts.results)
     return max(ts_width, maximum(child_widths))
 end
 
@@ -187,11 +190,11 @@ function filter_errors(ts::TimedTestSet)
     for t in ts.results
         if isa(t, Union{TimedTestSet,DefaultTestSet})
             append!(efs, filter_errors(t))
-        elseif isa(t, Union{Fail, Error})
+        elseif isa(t, Union{Fail,Error})
             append!(efs, [t])
         end
     end
-    efs
+    return efs
 end
 
 # Recursive function that counts the number of test results of each
@@ -200,13 +203,13 @@ function get_test_counts(ts::TimedTestSet)
     passes, fails, errors, broken = ts.n_passed, 0, 0, 0
     c_passes, c_fails, c_errors, c_broken = 0, 0, 0, 0
     for t in ts.results
-        isa(t, Fail)   && (fails  += 1)
-        isa(t, Error)  && (errors += 1)
+        isa(t, Fail) && (fails += 1)
+        isa(t, Error) && (errors += 1)
         isa(t, Broken) && (broken += 1)
         if isa(t, Union{TimedTestSet,DefaultTestSet})
-            np, nf, ne, nb, ncp, ncf, nce , ncb = get_test_counts(t)
+            np, nf, ne, nb, ncp, ncf, nce, ncb = get_test_counts(t)
             c_passes += np + ncp
-            c_fails  += nf + ncf
+            c_fails += nf + ncf
             c_errors += ne + nce
             c_broken += nb + ncb
         end
@@ -229,7 +232,7 @@ function print_counts(ts::TimedTestSet, depth, align,
 
     np = passes + c_passes
     if np > 0
-        printstyled(lpad(string(np), pass_width, " "), "  ", color=:green)
+        printstyled(lpad(string(np), pass_width, " "), "  "; color=:green)
     elseif pass_width > 0
         # No passes at this level, but some at another level
         print(lpad(" ", pass_width), "  ")
@@ -237,7 +240,7 @@ function print_counts(ts::TimedTestSet, depth, align,
 
     nf = fails + c_fails
     if nf > 0
-        printstyled(lpad(string(nf), fail_width, " "), "  ", color=Base.error_color())
+        printstyled(lpad(string(nf), fail_width, " "), "  "; color=Base.error_color())
     elseif fail_width > 0
         # No fails at this level, but some at another level
         print(lpad(" ", fail_width), "  ")
@@ -245,7 +248,7 @@ function print_counts(ts::TimedTestSet, depth, align,
 
     ne = errors + c_errors
     if ne > 0
-        printstyled(lpad(string(ne), error_width, " "), "  ", color=Base.error_color())
+        printstyled(lpad(string(ne), error_width, " "), "  "; color=Base.error_color())
     elseif error_width > 0
         # No errors at this level, but some at another level
         print(lpad(" ", error_width), "  ")
@@ -253,16 +256,16 @@ function print_counts(ts::TimedTestSet, depth, align,
 
     nb = broken + c_broken
     if nb > 0
-        printstyled(lpad(string(nb), broken_width, " "), "  ", color=Base.warn_color())
+        printstyled(lpad(string(nb), broken_width, " "), "  "; color=Base.warn_color())
     elseif broken_width > 0
         # None broken at this level, but some at another level
         print(lpad(" ", broken_width), "  ")
     end
 
     if np == 0 && nf == 0 && ne == 0 && nb == 0
-        printstyled("No tests", color=Base.info_color())
+        printstyled("No tests"; color=Base.info_color())
     else
-        printstyled(lpad(string(subtotal), total_width, " "), color=Base.info_color())
+        printstyled(lpad(string(subtotal), total_width, " "); color=Base.info_color())
     end
     println()
 
@@ -271,7 +274,7 @@ function print_counts(ts::TimedTestSet, depth, align,
         for t in ts.results
             if isa(t, Union{TimedTestSet,DefaultTestSet})
                 print_counts(t, depth + 1, align,
-                    pass_width, fail_width, error_width, broken_width, total_width)
+                             pass_width, fail_width, error_width, broken_width, total_width)
             end
         end
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,16 @@ using TestExtras
 using Test
 
 @timedtestset "constinferred tests" begin
-
-    mysqrt(x; complex = true) = x >= 0 ? sqrt(x) : (complex ? im*sqrt(-x) : throw(DomainError(x, "Enable complex return values to take square roots of negative numbers")))
+    function mysqrt(x; complex=true)
+        return x >= 0 ? sqrt(x) :
+               (complex ? im * sqrt(-x) :
+                throw(DomainError(x,
+                                  "Enable complex return values to take square roots of negative numbers")))
+    end
 
     @constinferred mysqrt(+3)
     @constinferred mysqrt(-3)
-    for x = -1.5:0.5:+1.5
+    for x in -1.5:0.5:+1.5
         @constinferred mysqrt($x)
         @constinferred mysqrt($(rand() < 0 ? x : -x))
     end
@@ -16,7 +20,7 @@ using Test
     @constinferred Nothing iterate(1:-1)
     @constinferred Tuple{Int,Int} iterate(1:-1)
 
-    x = (2,3)
+    x = (2, 3)
     @constinferred +(1, x...)
 
     @constinferred (1:3)[2]
@@ -24,9 +28,9 @@ using Test
     b = (3, 2, 1)
     @constinferred a .+ b
 
-    x = 3.
-    @constinferred mysqrt(x; complex = false)
-    @constinferred_broken mysqrt(x; complex = true)
+    x = 3.0
+    @constinferred mysqrt(x; complex=false)
+    @constinferred_broken mysqrt(x; complex=true)
 end
 
 # ensure constinferred only evaluates argument once
@@ -34,20 +38,18 @@ inferred_test_global = 0
 function inferred_test_function()
     global inferred_test_global
     inferred_test_global += 1
-    true
+    return true
 end
 @constinferred inferred_test_function()
 @test inferred_test_global == 1
 
-
-
-f25835(;x=nothing) = _f25835(x)
+f25835(; x=nothing) = _f25835(x)
 _f25835(::Nothing) = ()
 _f25835(x) = (x,)
 # A keyword function that is never type stable
-g25835(;x=1) = rand(Bool) ? 1.0 : 1
+g25835(; x=1) = rand(Bool) ? 1.0 : 1
 # A keyword function that is sometimes type stable
-h25835(;x=1,y=1) = x isa Int ? x*y : (rand(Bool) ? 1.0 : 1)
+h25835(; x=1, y=1) = x isa Int ? x * y : (rand(Bool) ? 1.0 : 1)
 
 @timedtestset begin
     @test @constinferred(f25835()) == ()
@@ -56,8 +58,8 @@ h25835(;x=1,y=1) = x isa Int ? x*y : (rand(Bool) ? 1.0 : 1)
     @test @constinferred(f25835(x=1)) == (1,)
 
     @test @constinferred(h25835()) == 1
-    @test @constinferred(h25835(x=2,y=3)) == 6
-    @test @constinferred(Union{Float64,Int64}, h25835(x=1.0,y=1.0)) == 1
+    @test @constinferred(h25835(x=2, y=3)) == 6
+    @test @constinferred(Union{Float64,Int64}, h25835(x=1.0, y=1.0)) == 1
 end
 
 @timedtestset "@test" begin
@@ -66,7 +68,7 @@ end
     @test 1 != 2
     @test ==(1, 1)
     @test ==((1, 1)...)
-    @test 1 ≈ 2 atol=1
+    @test 1 ≈ 2 atol = 1
     @test strip("\t  hi   \n") == "hi"
     @test strip("\t  this should fail   \n") != "hi"
     @test isequal(1, 1)
@@ -76,8 +78,8 @@ end
 end
 @timedtestset "@test keyword precedence" begin
     # post-semicolon keyword, suffix keyword, pre-semicolon keyword
-    @test isapprox(1, 2, atol=0) atol=1
-    @test isapprox(1, 3, atol=0; atol=2) atol=1
+    @test isapprox(1, 2, atol=0) atol = 1
+    @test isapprox(1, 3, atol=0; atol=2) atol = 1
 end
 @timedtestset "@test should only evaluate the arguments once" begin
     g = Int[]
@@ -109,7 +111,10 @@ end
 end
 @timedtestset "@test_warn" begin
     @test 1234 === @test_nowarn(1234)
-    @test 5678 === @test_warn("WARNING: foo", begin println(stderr, "WARNING: foo"); 5678; end)
+    @test 5678 === @test_warn("WARNING: foo", begin
+                                  println(stderr, "WARNING: foo")
+                                  5678
+                              end)
     let a
         @test_throws UndefVarError(:a) a
         @test_nowarn a = 1
